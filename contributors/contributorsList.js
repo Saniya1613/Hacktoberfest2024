4d79823298b90c129ad19cdde37b22c2e56892a4
Saniya Patil <saniyagranger66@gmail.com>
2024-10-22 22:20:32 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+
+  {
+    id: 428,
+    fullname: "Saniya Patil",
+    username: "https://github.com/Saniya1613",
+  },
   
 ];
